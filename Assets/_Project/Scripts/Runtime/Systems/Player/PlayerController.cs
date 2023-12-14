f04663c2f57de816ade7a3bf13f27dd770e3748a
Erick Araujo <erickequipe7@gmail.com>
2023-12-14 11:22:42 -0300

    Added player base

---
+++ Assets/_Project/Scripts/Runtime/Systems/Player/PlayerController.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D playerRb;
+    private int idAnim;
+    private float speedY;
+    private bool isGrounded;
+    private bool isLeft;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Vector2 boxSize;
+    [SerializeField] private LayerMask groundMask;
+
+    private float horizontal;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, boxSize, 0, groundMask);
+    }
+
+    private void Update()
+    {       
+        UpdateAnimator();
+        Jump();
+        Move();
+       
+    }
+
+    private void Move()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        playerRb.velocity = new(horizontal * speed, speedY);
+
+        if (horizontal != 0)
+        {
+            idAnim = 2;
+        }
+        else
+        {
+            idAnim = 0;
+        }
+
+        if (isLeft && horizontal > 0)
+        {
+            Flip();
+        }
+
+        if (!isLeft && horizontal < 0)
+        {
+            Flip();
+        }
+
+    }
+
+    private void Jump()
+    {
+        speedY = playerRb.velocity.y;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerRb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        animator.SetFloat("speedY", speedY);
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetInteger("idAnim", idAnim);
+    }
+
+
+    private void Flip()
+    {
+        isLeft = !isLeft;
+        float scaleX = transform.localScale.x;
+        scaleX *= -1f;
+        transform.localScale = new(scaleX, transform.localScale.y, transform.localScale.z);
+
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(groundCheck.position, boxSize);
+    }
+#endif
+}
